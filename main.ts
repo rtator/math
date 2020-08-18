bafc0f22a8c1a76aa495603f9aa1f2209df3941a
rtator <66912559+rtator@users.noreply.github.com>
2020-08-18 10:36:30 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -4,15 +4,102 @@ function change_level () {
         _2 = randint(5, 10)
         answer = _1 * _2
         player_answer = game.askForNumber("" + _1 + " x " + _2)
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else if (false) {
-    	
+        if (player_answer == answer) {
+            game.showLongText("right!", DialogLayout.Bottom)
+            if (game.ask("try again?")) {
+                change_level()
+            } else {
+                level += 1
+                change_level()
+            }
+        } else {
+            game.showLongText("wrong.", DialogLayout.Bottom)
+            change_level()
+        }
+    } else if (level == 2) {
+        _1 = randint(11, 15)
+        _2 = randint(11, 15)
+        answer = _1 * _2
+        player_answer = game.askForNumber("" + _1 + " x " + _2)
+        if (player_answer == answer) {
+            game.showLongText("right!", DialogLayout.Bottom)
+            if (game.ask("try again?")) {
+                change_level()
+            } else {
+                level += 1
+                change_level()
+            }
+        } else {
+            game.showLongText("wrong.", DialogLayout.Bottom)
+            change_level()
+        }
+    } else if (level == 3) {
+        _1 = randint(16, 20)
+        _2 = randint(16, 20)
+        answer = _1 * _2
+        player_answer = game.askForNumber("" + _1 + " x " + _2)
+        if (player_answer == answer) {
+            game.showLongText("right!", DialogLayout.Bottom)
+            if (game.ask("try again?")) {
+                change_level()
+            } else {
+                level += 1
+                change_level()
+            }
+        } else {
+            game.showLongText("wrong.", DialogLayout.Bottom)
+            change_level()
+        }
+    } else if (level == 4) {
+        _1 = randint(21, 25)
+        _2 = randint(21, 25)
+        answer = _1 * _2
+        player_answer = game.askForNumber("" + _1 + " x " + _2)
+        if (player_answer == answer) {
+            game.showLongText("right!", DialogLayout.Bottom)
+            if (game.ask("try again?")) {
+                change_level()
+            } else {
+                level += 1
+                change_level()
+            }
+        } else {
+            game.showLongText("wrong.", DialogLayout.Bottom)
+            change_level()
+        }
+    } else if (level == 5) {
+        _1 = randint(26, 30)
+        _2 = randint(26, 30)
+        answer = _1 * _2
+        player_answer = game.askForNumber("" + _1 + " x " + _2)
+        if (player_answer == answer) {
+            game.showLongText("right!", DialogLayout.Bottom)
+            if (game.ask("try again?")) {
+                change_level()
+            } else {
+                test()
+            }
+        } else {
+            game.showLongText("wrong.", DialogLayout.Bottom)
+            change_level()
+        }
     }
+}
+function test () {
+    game.splash("test")
+    for (let index = 0; index < 4; index++) {
+        _1 = randint(5, 30)
+        _2 = randint(5, 30)
+        answer = _1 * _2
+        player_answer = game.askForNumber("" + _1 + " x " + _2)
+        if (player_answer == answer) {
+            game.showLongText("right!", DialogLayout.Bottom)
+            info.changeScoreBy(1)
+        } else {
+            game.showLongText("wrong.", DialogLayout.Bottom)
+        }
+    }
+    game.over(true)
 }
 let player_answer = 0
 let answer = 0
