c70ec37cb8dcb819568dca91ae877e02ccff5337
rtator <66912559+rtator@users.noreply.github.com>
2020-08-18 10:23:29 -0700

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,24 @@
-
+function change_level () {
+    if (level == 1) {
+        _1 = randint(5, 10)
+        _2 = randint(5, 10)
+        answer = _1 * _2
+        player_answer = game.askForNumber("" + _1 + " x " + _2)
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    }
+}
+let player_answer = 0
+let answer = 0
+let _2 = 0
+let _1 = 0
+let level = 0
+scene.setBackgroundColor(9)
+level = 1
+change_level()
